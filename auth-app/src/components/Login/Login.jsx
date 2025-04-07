d7572504b09919210e11d718df005befab6f83c3
mohsin_ali <mohsin512syed@gmail.com>
2025-04-07 11:26:56 +0500

    login page style

--- auth-app/src/components/Login/Login.jsx
+++ auth-app/src/components/Login/Login.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styles from './Login.module.css'
+import InputControl from '../InputControl/InputControl'
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <div className={styles.innerBox}>
+        <h1 className={styles.heading}>login</h1>
+        <InputControl label="Email" placeholder="Enter Your Email" />
+        <InputControl label="Password" placeholder="Enter your Password" />
+        <div className={styles.footer}>
+          <button>Login</button>
+          <p>
+            Already have an account?{""}
+            <span>
+              <Link to="/Signup">Sign up</Link>
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- auth-app/src/components/Signup/Signup.jsx
+++ auth-app/src/components/Signup/Signup.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Signup() {
   return (
     <div>
-      
+      <h1>singup</h1>
     </div>
   )
 }
